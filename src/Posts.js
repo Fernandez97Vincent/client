92d1233044204362f9eec6ff22a4605685d44343
Vincent Fernandez <fernandez97vincent@gmail.com>
2023-03-19 00:40:35 -0700

    yes

--- src/Posts.js
+++ src/Posts.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 
 function Posts() {
+    const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
@@ -27,17 +29,38 @@ function Posts() {
     return (
         <div>
             <h1>Hello</h1>
+            <Button onClick={() => navigate(-1)}>Back</Button>
+            
             {posts ? (
                 <>
                     {posts.map(post => {
                         return(
-                            <div>
-                                key={post._id}
+                            <div
+                            style={{display:"inline-block", padding:"2rem"}}
+                            >
                                 <h4>{post.title}</h4>
-                                <p>{post.description}</p>
+                                <p>Shift: {post.shift}</p>
+                                <h2>Kitchen</h2>
+                                <p>George: {post.george}</p>
+                                <p>Horacio: {post.horacio}</p>
+                                <p>Rafaela: {post.rafaela}</p>
+                                <p>Catalina: {post.catalina}</p>
+                                <p>Ranulfo: {post.ranulfo}</p>
+                                <h2>Sushi Bar</h2>
+                                <p>James: {post.james}</p>
+                                <p>Bruce: {post.bruce}</p>
+                                <p>Gabe: {post.gabe}</p>
+                                <p>Freddie: {post.freddie}</p>
+                                <h2>FoH</h2>
+                                <p>Vincent: {post.vincent}</p>
+                                <p>Bam: {post.bam}</p>
+                                <p>yoshiko: {post.yoshiko}</p>
+                                <p>Josh: {post.josh}</p>
                                 <Button>UPDATE</Button>
                                 <Button onClick={() => deletePost(post._id)}>DELETE</Button>
+                                
                             </div>
+                            
                         );
                     })}
                 </>
